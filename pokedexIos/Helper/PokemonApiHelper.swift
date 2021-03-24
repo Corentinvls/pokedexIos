90d0110b7a534dc811e29ee15d6968aed9cc1839
Lexo783 <florentdebuchy@orange.fr>
2021-03-24 16:24:17 +0100

    fix helper
    c'est pas vraiment un helper si on peut pas faire des tableaux "Classique"????
    
    :yokotaroyeslife:

--- pokedexIos/Helper/PokemonApiHelper.swift
+++ pokedexIos/Helper/PokemonApiHelper.swift
@@ -16,31 +16,49 @@ class PokedexApiHelper {
     
     func getAllPokemon(
         completion: @escaping (_ data:[PokemonUrl], _ error:Error?) -> Void) {
-            PokemonAPI().pokemonService.fetchPokemonList(){
+        PokemonAPI().pokemonService.fetchPokemonList(){
             result in
-                var pokemonsData: [PokemonUrl] = []
+            var pokemonsData: [PokemonUrl] = []
             switch result {
-                case .success(let pokemons):
-                    for pokemon in pokemons.results! {
-                        pokemonsData.append(PokemonUrl.init(pokemon: pokemon)!)
-                    }
-                    completion(pokemonsData, nil)
-                    break
-                case .failure(let error):
-                    completion([],error)
-                    break
+            case .success(let pokemons):
+                for pokemon in pokemons.results! {
+                    // Ajout de l'url dans le model pokemon url
+                    pokemonsData.append(PokemonUrl.init(pokemon: pokemon)!)
+                }
+                completion(pokemonsData,nil)
+                break
+            case .failure(let error):
+                completion([],error)
+                break
             }
         }
     }
     
-    func test(url:String){
+    func callUrl(
+        url:String,
+        completion: @escaping (_ data:[String:AnyObject], _ error:Error?) -> Void) {
+        
         let url = URL(string: url)!
         URLSession.shared.dataTask(with: url)
+        
         let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
-            print(data)
+            
+            if let json = try? JSONSerialization.jsonObject(with: data!, options: .mutableContainers) {
+                if let data = json as? [String: AnyObject] {
+                    if(error == nil){
+                        completion(data,nil)
+                    }
+                    else{
+                        completion([:],error)
+                    }
+                    
+                }
+                
+            }
         })
-
+        
         task.resume()
     }
-
+    
+    
 }
--- pokedexIos/ViewController.swift
+++ pokedexIos/ViewController.swift
@@ -11,13 +11,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        PokedexApiHelper.shared.getAllPokemon { (pokemons, error) in
-            print(pokemons)
-        }
         
-        PokedexApiHelper.shared.test(
-            url:"https://pokeapi.co/api/v2/pokemon/1/")
-        // Do any additional setup after loading the view.
+        PokedexApiHelper.shared.getAllPokemon {
+            (pokemons, error) in
+            print(pokemons)
+            for pokemon in pokemons{
+                if let abilities = pokemon["abilities"] as? [[String: AnyObject]] {
+                    
+                    for abililite in abilities{
+                        print(abililite)
+                    }
+                }
+            }
+            /*
+            PokedexApiHelper.shared.callUrl(
+                url:"https://pokeapi.co/api/v2/pokemon/16/") {
+                (data, error) in
+                print(data["name"])
+            }*/
+        }
     }
     
     
